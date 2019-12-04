f9d319073f511e0389d69188127754ba44082d32
mRcfps <1402491442@qq.com>
2019-12-04 09:49:43 +0800

    Node 全局对象初探

--- timer.js
+++ timer.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+setTimeout(() => {
+  console.log('Hello World!');
+}, 3000);
+
+console.log('当前进程 ID', process.pid);
+console.log('当前脚本路径', __filename);
+
+const time = new Date();
+console.log('当前时间', time.toLocaleString());
