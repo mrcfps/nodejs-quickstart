0658c65e87d60c80566e1af367488685eab5bfcc
mRcfps <1402491442@qq.com>
2019-12-05 20:40:50 +0800

    命令行开发：接受输入参数

--- timer.js
+++ timer.js
@@ -1,9 +1,12 @@
 const printProgramInfo = require('./info');
 const datetime = require('./datetime');
 
+const waitTime = Number(process.argv[3]);
+const message = process.argv[5];
+
 setTimeout(() => {
-  console.log('Hello World!');
-}, 3000);
+  console.log(message);
+}, waitTime * 1000);
 
 printProgramInfo();
 console.log('当前时间', datetime.getCurrentTime());
