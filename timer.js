f2a1b0c36f32444c2e3d1652630a73ddb84563e8
mRcfps <1402491442@qq.com>
2019-12-05 22:45:54 +0800

    npm：洪荒之力，都赐予你

--- timer.js
+++ timer.js
@@ -1,12 +1,18 @@
+const program = require('commander');
+const ora = require('ora');
 const printProgramInfo = require('./info');
 const datetime = require('./datetime');
 
-const waitTime = Number(process.argv[3]);
-const message = process.argv[5];
+program
+  .option('-t, --time <number>', '等待时间 (秒)', 3)
+  .option('-m, --message <string>', '要输出的信息', 'Hello World')
+  .parse(process.argv);
 
 setTimeout(() => {
-  console.log(message);
-}, waitTime * 1000);
+  spinner.stop();
+  console.log(program.message);
+}, program.time * 1000);
 
 printProgramInfo();
 console.log('当前时间', datetime.getCurrentTime());
+const spinner = ora('正在加载中，请稍后 ...').start();
